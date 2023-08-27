980d62ca39f971a1fde93ff2c28542b23879f910
Sujith <118133606+sujith2903@users.noreply.github.com>
2023-08-27 12:43:59 -0500

    Returns number of steps and path

--- knights-travails.js
+++ knights-travails.js
@@ -11,7 +11,6 @@ const possibleMoves = function ([u, v]) {
                 if (nextU >= 0 && nextU <= 7) {
                     moves.push(nextU)
                 }
-
                 let nextV = v + j
                 if (nextV >= 0 && nextV <= 7) {
                     moves.push(nextV)
@@ -25,7 +24,39 @@ const possibleMoves = function ([u, v]) {
     return possibleMovesArray
 }
 
-const knightsTravails = function ([a, b], [x, y]) {
+const node = function ([a, b]) {
     
+    let path = []
+    let currentPosition = [a, b]
+    let nextPositions = possibleMoves([a, b])
     
+    return {path,currentPosition,nextPositions}
 }
+
+let queue = []
+
+const knightsTravails = function ([a, b], [x, y]) {
+
+    let knight = node([a, b])
+    queue.push(knight)
+
+    while (queue.length > 0) {
+        let current = queue.shift()
+        current.path.push([current.currentPosition[0], current.currentPosition[1]])
+
+        if (current.currentPosition[0] == x && current.currentPosition[1] == y) {
+            console.log(`You have reached the target in ${current.path.length-1} steps in the path : `)
+            return current.path
+        }
+
+        current.nextPositions.forEach((position) => {
+            let newNode = node(position)
+            queue.push(newNode)
+            current.path.forEach((path) => {
+                newNode.path.push(path)
+            })
+        })
+    }
+}
+
+
